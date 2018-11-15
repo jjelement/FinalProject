dc04d376e023357ae64ae54a501e2674dc7d646e
JJELEMENT <jjelementza@gmail.com>
2018-11-16 02:37:49 +0700

    init

--- src/java/Project/model/servlet/CheckoutServlet.java
+++ src/java/Project/model/servlet/CheckoutServlet.java
@@ -1,7 +1,6 @@
 package Project.model.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
--- src/java/Project/model/servlet/RegisterServlet.java
+++ src/java/Project/model/servlet/RegisterServlet.java
@@ -6,7 +6,6 @@
 package Project.model.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
